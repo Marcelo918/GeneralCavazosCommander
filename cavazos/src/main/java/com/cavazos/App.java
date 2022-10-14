7419af08838d704e8d63df13fc1ea89dc81906f9
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2022-10-14 10:35:01 -0700

    Task 4 completed. Added the menu.

--- cavazos/src/main/java/com/cavazos/App.java
+++ cavazos/src/main/java/com/cavazos/App.java
@@ -8,6 +8,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //The following print statements is the menu
+        System.out.println( "--------------------------------------------" );
+        System.out.println("General Cavazos Commander App");
+        System.out.println("--------------------------------------------");
+        System.out.println("i     Issue a comand");
+        System.out.println("l     List all of the commands");
+        System.out.println("u     Undo the last command that was issued");
+        System.out.println("r     Redo the last command that was issued");
+        System.out.println("q     Quit");
+        System.out.println( "--------------------------------------------" );
+
+
     }
 }
